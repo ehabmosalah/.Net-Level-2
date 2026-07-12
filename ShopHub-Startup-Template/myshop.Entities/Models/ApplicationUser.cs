3306f2362ca1b3f231a7bd84482bf281fe4011e7
Ehab Mohamed Salah Ali <ehabmo.salah2005@gmail.com>
2026-07-12 21:36:36 +0300

    Return the ApplicationUser back

--- ShopHub-Startup-Template/myshop.Entities/Models/ApplicationUser.cs
+++ ShopHub-Startup-Template/myshop.Entities/Models/ApplicationUser.cs
@@ -14,6 +14,6 @@ namespace myshop.Entities.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
-        public string FullName { get; set; }
+        // public string FullName { get; set; }
     }
 }
